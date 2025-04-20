5caadf18c25a0c2f32202a6a7cee7cb6d49488db
juanmaalvide <juanmanuelalvide@gmail.com>
2025-04-20 20:15:53 -0300

    Commit con ejercicio num 3 de Reportes

--- CAI_ejericico_login/CAI_ejericico_login/FormReportes.cs
+++ CAI_ejericico_login/CAI_ejericico_login/FormReportes.cs
@@ -53,16 +53,45 @@ namespace CAI_ejericico_login
 
         private void btnGenerar3_Click(object sender, EventArgs e)
         {
-            List <FormReportes> CarrerasAlumno (int idAlumno)
-            {
-                Alumno alumno = buscarAlumno (idAlumno);
 
-            }
+            var alumnosFiltrados = FiltrarAlumnosConMultiplesCarreras(uba.Alumnos);
+            var alumno = ObtenerAlumnoConMasAprobadas(alumnosFiltrados);
+
+            string nombreAlumno = alumno.Nombre;
+            string nombreCarreras = String.Join("", alumno.Carreras);
+            List<int> materiasAprobadas = ObtenerMateriasAprobadasPorAlumno(alumno);
+
         }
-        
 
-    
-    
+        public List<Alumno> FiltrarAlumnosConMultiplesCarreras(List<Alumno> alumnos)
+        {
+            return alumnos.Where(a => a.Carreras.Count > 1).ToList();
+        }
+
+        public List<int> ObtenerMateriasAprobadasPorAlumno(Alumno alumno)
+        {
+                        //Necesito obtener las materias aprobadas por alumno
+            List<int> MateriasAprobadas = new List<int>();
+            foreach (var Examen in alumno.Examenes)
+            {           //recorro todos sus examenes 
+                if (Examen.Nota >= 4)
+                {
+                        //si saco mas de 4 agrego el id de la materia que aprobo
+                    MateriasAprobadas.Add(Examen.IdMateria);
+                }
+            }
+                        //devuelvo los id de las materias aprobadas
+            return MateriasAprobadas;
+        }
+
+        public Alumno ObtenerAlumnoConMasAprobadas(List<Alumno> alumnos)
+        {
+            return alumnos
+                .OrderByDescending(a => ObtenerMateriasAprobadasPorAlumno(a).Count)
+                .FirstOrDefault();
+
+        }
+
     }
 
 
